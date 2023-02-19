86161753ba2ba6f48ec2311ac55a7822dd10c18e
Valentin REVERSAT <dev@vareversat.fr>
2023-02-19 22:55:50 +0100

    chore(theme): use default theme for both light and dark

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -2,41 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class AppThemes {
-  static final brightTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.white,
-    primarySwatch: Colors.blue,
+  static final brightTheme = ThemeData.light(
     useMaterial3: true,
-    textTheme: GoogleFonts.josefinSansTextTheme(),
-    colorScheme: const ColorScheme(
-        brightness: Brightness.light,
-        primary: Color(0xff6200ee),
-        onPrimary: Colors.white,
-        secondary: Color(0xff03dac6),
-        onSecondary: Colors.black,
-        error: Color(0xffb00020),
-        onError: Colors.white,
-        background: Colors.white,
-        onBackground: Colors.black,
-        surface: Colors.white,
-        onSurface: Colors.black),
+  ).copyWith(
+    textTheme: GoogleFonts.josefinSansTextTheme(
+      ThemeData.light().textTheme,
+    ),
   );
 
-  static final darkTheme = ThemeData(
-    scaffoldBackgroundColor: Colors.black45,
-    primarySwatch: Colors.teal,
+  static final darkTheme = ThemeData.dark(
     useMaterial3: true,
-    textTheme: GoogleFonts.josefinSansTextTheme(),
-    colorScheme: const ColorScheme(
-        brightness: Brightness.dark,
-        primary: Color(0xffbb86fc),
-        onPrimary: Colors.black,
-        secondary: Color(0xff03dac6),
-        onSecondary: Colors.black,
-        error: Color(0xffcf6679),
-        onError: Colors.white,
-        background: Colors.black,
-        onBackground: Colors.white,
-        surface: Colors.black,
-        onSurface: Colors.white),
+  ).copyWith(
+    scaffoldBackgroundColor: Colors.black45,
+    textTheme: GoogleFonts.josefinSansTextTheme(
+      ThemeData.dark().textTheme,
+    ),
   );
 }
