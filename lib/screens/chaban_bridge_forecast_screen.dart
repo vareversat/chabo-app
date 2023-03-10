19615118c394ee17e3daebd9c20c7cb9fab11d5c
Valentin REVERSAT <dev@vareversat.fr>
2023-03-10 19:34:45 +0100

    feat(ui): remove the app title and use a `SafeArea` instead

--- lib/screens/chaban_bridge_forecast_screen.dart
+++ lib/screens/chaban_bridge_forecast_screen.dart
@@ -3,7 +3,6 @@ import 'package:chabo/bloc/duration_picker/duration_picker_bloc.dart';
 import 'package:chabo/bloc/notification_service_cubit.dart';
 import 'package:chabo/bloc/scroll_status/scroll_status_bloc.dart';
 import 'package:chabo/bloc/time_picker/time_picker_bloc.dart';
-import 'package:chabo/const.dart';
 import 'package:chabo/custom_widgets_state.dart';
 import 'package:chabo/models/chaban_bridge_status.dart';
 import 'package:chabo/screens/error_screen.dart';
@@ -30,9 +29,6 @@ class _ChabanBridgeForecastScreenState
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(Const.appName),
-      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
@@ -45,104 +41,109 @@ class _ChabanBridgeForecastScreenState
         heroTag: 'settingsButtonIcon',
         child: const Icon(Icons.settings),
       ),
-      body: BlocBuilder<ChabanBridgeForecastBloc, ChabanBridgeForecastState>(
-        builder: (context, state) {
-          switch (state.status) {
-            case ChabanBridgeForecastStatus.failure:
-              return ErrorScreen(errorMessage: state.message);
-            case ChabanBridgeForecastStatus.success:
-              if (state.chabanBridgeForecasts.isEmpty) {
-                return const ErrorScreen(errorMessage: 'Empty return');
-              }
-              var bridgeStatus = ChabanBridgeStatus(
-                  currentChabanBridgeForecast:
-                      state.currentChabanBridgeForecast!,
-                  context: context);
-
-              void scrollToSchedule() async {
-                if (_scrollController.hasClients) {
-                  final itemCount = state.chabanBridgeForecasts.length;
-                  final indexGoTo = state.chabanBridgeForecasts
-                      .indexOf(state.currentChabanBridgeForecast!);
-                  final contentSize =
-                      _scrollController.position.viewportDimension +
-                          _scrollController.position.maxScrollExtent;
-                  final target = contentSize * indexGoTo / itemCount;
-                  _scrollController.position.animateTo(
-                    target,
-                    duration: const Duration(seconds: 1),
-                    curve: Curves.ease,
-                  );
-                  BlocProvider.of<ScrollStatusBloc>(context).add(
-                    ScrollStatusChanged(type: ScrollStatusStateType.automatic),
-                  );
+      body: SafeArea(
+        child: BlocBuilder<ChabanBridgeForecastBloc, ChabanBridgeForecastState>(
+          builder: (context, state) {
+            switch (state.status) {
+              case ChabanBridgeForecastStatus.failure:
+                return ErrorScreen(errorMessage: state.message);
+              case ChabanBridgeForecastStatus.success:
+                if (state.chabanBridgeForecasts.isEmpty) {
+                  return const ErrorScreen(errorMessage: 'Empty return');
                 }
-              }
+                var bridgeStatus = ChabanBridgeStatus(
+                    currentChabanBridgeForecast:
+                        state.currentChabanBridgeForecast!,
+                    context: context);
 
-              SchedulerBinding.instance.addPostFrameCallback(
-                (_) {
-                  scrollToSchedule();
-                },
-              );
-              return MultiBlocListener(
-                listeners: [
-                  BlocListener<DurationPickerBloc, DurationPickerState>(
-                    listener: (context, state) {
-                      context
-                          .read<NotificationServiceCubit>()
-                          .state
-                          .computeDurationScheduledNotifications(
-                              BlocProvider.of<ChabanBridgeForecastBloc>(context)
-                                  .state
-                                  .chabanBridgeForecasts,
-                              state,
-                              context);
-                    },
-                  ),
-                  BlocListener<TimePickerBloc, TimePickerState>(
-                    listener: (context, state) {
-                      context
-                          .read<NotificationServiceCubit>()
-                          .state
-                          .computeTimeScheduledNotifications(
-                              BlocProvider.of<ChabanBridgeForecastBloc>(context)
-                                  .state
-                                  .chabanBridgeForecasts,
-                              state,
-                              context);
-                    },
-                  )
-                ],
-                child: Column(
-                  children: [
-                    BlocBuilder<ScrollStatusBloc, ScrollStatusState>(
-                      builder: (context, state) {
-                        return ChabanBridgeStatusWidget(
-                          showCurrentStatus: state.isOnCurrentScheduleLevel,
-                          onTap: scrollToSchedule,
-                          bridgeStatus: bridgeStatus,
-                        );
+                void scrollToSchedule() async {
+                  if (_scrollController.hasClients) {
+                    final itemCount = state.chabanBridgeForecasts.length;
+                    final indexGoTo = state.chabanBridgeForecasts
+                        .indexOf(state.currentChabanBridgeForecast!);
+                    final contentSize =
+                        _scrollController.position.viewportDimension +
+                            _scrollController.position.maxScrollExtent;
+                    final target = contentSize * indexGoTo / itemCount;
+                    _scrollController.position.animateTo(
+                      target,
+                      duration: const Duration(seconds: 1),
+                      curve: Curves.ease,
+                    );
+                    BlocProvider.of<ScrollStatusBloc>(context).add(
+                      ScrollStatusChanged(
+                          type: ScrollStatusStateType.automatic),
+                    );
+                  }
+                }
+
+                SchedulerBinding.instance.addPostFrameCallback(
+                  (_) {
+                    scrollToSchedule();
+                  },
+                );
+                return MultiBlocListener(
+                  listeners: [
+                    BlocListener<DurationPickerBloc, DurationPickerState>(
+                      listener: (context, state) {
+                        context
+                            .read<NotificationServiceCubit>()
+                            .state
+                            .computeDurationScheduledNotifications(
+                                BlocProvider.of<ChabanBridgeForecastBloc>(
+                                        context)
+                                    .state
+                                    .chabanBridgeForecasts,
+                                state,
+                                context);
                       },
                     ),
-                    Expanded(
-                      flex: 11,
-                      child: ChabanBridgeForecastList(
-                        scrollController: _scrollController,
-                        currentChabanBridgeForecast:
-                            state.currentChabanBridgeForecast,
-                        chabanBridgeForecasts: state.chabanBridgeForecasts,
-                        hasReachedMax: state.hasReachedMax,
-                      ),
-                    ),
+                    BlocListener<TimePickerBloc, TimePickerState>(
+                      listener: (context, state) {
+                        context
+                            .read<NotificationServiceCubit>()
+                            .state
+                            .computeTimeScheduledNotifications(
+                                BlocProvider.of<ChabanBridgeForecastBloc>(
+                                        context)
+                                    .state
+                                    .chabanBridgeForecasts,
+                                state,
+                                context);
+                      },
+                    )
                   ],
-                ),
-              );
-            default:
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-          }
-        },
+                  child: Column(
+                    children: [
+                      BlocBuilder<ScrollStatusBloc, ScrollStatusState>(
+                        builder: (context, state) {
+                          return ChabanBridgeStatusWidget(
+                            showCurrentStatus: state.isOnCurrentScheduleLevel,
+                            onTap: scrollToSchedule,
+                            bridgeStatus: bridgeStatus,
+                          );
+                        },
+                      ),
+                      Expanded(
+                        flex: 11,
+                        child: ChabanBridgeForecastList(
+                          scrollController: _scrollController,
+                          currentChabanBridgeForecast:
+                              state.currentChabanBridgeForecast,
+                          chabanBridgeForecasts: state.chabanBridgeForecasts,
+                          hasReachedMax: state.hasReachedMax,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              default:
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+            }
+          },
+        ),
       ),
     );
   }
