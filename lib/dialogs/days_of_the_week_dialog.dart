e10b5c3d5aa87f9ca08e9f118348aaaa3c2f09d1
Valentin REVERSAT <dev@vareversat.fr>
2023-05-08 14:14:44 +0200

    chore(format): run `dart format lib`

--- lib/dialogs/days_of_the_week_dialog.dart
+++ lib/dialogs/days_of_the_week_dialog.dart
@@ -5,9 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class DaysOfTheWeekDialog extends StatelessWidget {
-
-  const DaysOfTheWeekDialog({Key? key})
-      : super(key: key);
+  const DaysOfTheWeekDialog({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
