82bf67e2bb4323f0c1285cd976509c99f554f86c
Valentin REVERSAT <dev@vareversat.fr>
2023-05-08 14:18:30 +0200

    feat(time-slot): forecast list now listen the TimeBloc

--- lib/widgets/forecast/forecast_list_widget.dart
+++ lib/widgets/forecast/forecast_list_widget.dart
@@ -1,5 +1,6 @@
 import 'package:chabo/bloc/chaban_bridge_forecast/chaban_bridge_forecast_bloc.dart';
 import 'package:chabo/bloc/scroll_status/scroll_status_bloc.dart';
+import 'package:chabo/bloc/time_slot/time_slot_bloc.dart';
 import 'package:chabo/models/abstract_chaban_bridge_forecast.dart';
 import 'package:chabo/widgets/ad_banner_widget.dart';
 import 'package:chabo/widgets/forecast/forecast_list_item_widget.dart';
@@ -30,45 +31,57 @@ class _ForecastListWidgetState extends State<ForecastListWidget> {
         return true;
       },
       child: BlocBuilder<ChabanBridgeForecastBloc, ChabanBridgeForecastState>(
-        builder: (context, state) {
-          return ListView.separated(
-            cacheExtent: 5000,
-            padding: const EdgeInsets.all(0),
-            itemBuilder: (BuildContext context, int index) {
-              return ForecastListItemWidget(
-                  key: GlobalObjectKey(
-                      state.chabanBridgeForecasts[index].hashCode),
-                  isCurrent: state.chabanBridgeForecasts[index] ==
-                      state.currentChabanBridgeForecast,
-                  hasPassed: state
-                      .chabanBridgeForecasts[index].circulationReOpeningDate
-                      .isBefore(DateTime.now()),
-                  chabanBridgeForecast: state.chabanBridgeForecasts[index],
-                  index: index,
-              isInterfering: index % 2 == 0,);
-            },
-            itemCount: state.chabanBridgeForecasts.length,
-            controller:
-                BlocProvider.of<ScrollStatusBloc>(context).scrollController,
-            separatorBuilder: (BuildContext context, int index) {
-              if ((index + 1 <= state.chabanBridgeForecasts.length &&
-                  state.chabanBridgeForecasts[index].circulationClosingDate
-                          .month !=
-                      state.chabanBridgeForecasts[index + 1]
-                          .circulationClosingDate.month)) {
-                return _MonthWidget(
-                  chabanBridgeForecast: state.chabanBridgeForecasts[index + 1],
-                );
-              }
-              if (((index % 10 == 0 ||
-                          index ==
-                              state.chabanBridgeForecasts.indexOf(
-                                  state.currentChabanBridgeForecast!)) &&
-                      index != 0) &&
-                  !kIsWeb) {
-                return const AdBannerWidget();
-              }
-              return const SizedBox.shrink();
+        builder: (context, forecastState) {
+          return BlocBuilder<TimeSlotBloc, TimeSlotState>(
+            builder: (context, timeSlotState) {
+              return ListView.separated(
+                cacheExtent: 5000,
+                padding: const EdgeInsets.all(0),
+                itemBuilder: (BuildContext context, int index) {
+                  forecastState.chabanBridgeForecasts[index]
+                      .checkSlotInterference(timeSlotState.timeSlots);
+                  return ForecastListItemWidget(
+                    key: GlobalObjectKey(
+                        forecastState.chabanBridgeForecasts[index].hashCode),
+                    isCurrent: forecastState.chabanBridgeForecasts[index] ==
+                        forecastState.currentChabanBridgeForecast,
+                    hasPassed: forecastState
+                        .chabanBridgeForecasts[index].circulationReOpeningDate
+                        .isBefore(DateTime.now()),
+                    chabanBridgeForecast:
+                        forecastState.chabanBridgeForecasts[index],
+                    index: index,
+                    timeSlots: forecastState
+                        .chabanBridgeForecasts[index].interferingTimeSlots,
+                  );
+                },
+                itemCount: forecastState.chabanBridgeForecasts.length,
+                controller:
+                    BlocProvider.of<ScrollStatusBloc>(context).scrollController,
+                separatorBuilder: (BuildContext context, int index) {
+                  if ((index + 1 <=
+                          forecastState.chabanBridgeForecasts.length &&
+                      forecastState.chabanBridgeForecasts[index]
+                              .circulationClosingDate.month !=
+                          forecastState.chabanBridgeForecasts[index + 1]
+                              .circulationClosingDate.month)) {
+                    return _MonthWidget(
+                      chabanBridgeForecast:
+                          forecastState.chabanBridgeForecasts[index + 1],
+                    );
+                  }
+                  if (((index % 10 == 0 ||
+                              index ==
+                                  forecastState.chabanBridgeForecasts.indexOf(
+                                      forecastState
+                                          .currentChabanBridgeForecast!)) &&
+                          index != 0) &&
+                      !kIsWeb) {
+                    return const AdBannerWidget();
+                  }
+                  return const SizedBox.shrink();
+                },
+              );
             },
           );
         },
