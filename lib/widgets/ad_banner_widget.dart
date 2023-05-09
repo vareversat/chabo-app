13e4ef78c1a13429366e4f54152280ce72f600f5
Valentin REVERSAT <dev@vareversat.fr>
2023-05-09 21:44:03 +0200

    Revert "test"
    This reverts commit 0e68fb464c78a39c0f836f205f5f3540eac30067.

--- lib/widgets/ad_banner_widget.dart
+++ lib/widgets/ad_banner_widget.dart
@@ -15,7 +15,6 @@ class AdBannerWidget extends StatefulWidget {
 
 class _AdBannerWidgetState extends State<AdBannerWidget> {
   late NativeAd _bannerAd;
-  late BannerAd _bannerAd2;
   Ad? _ad;
 
   void _createBannerAd() {
@@ -43,34 +42,9 @@ class _AdBannerWidgetState extends State<AdBannerWidget> {
     _bannerAd.load();
   }
 
-  void _createBannerAd2() {
-    _bannerAd2 = BannerAd(
-      adUnitId: AdHelper.inlineBannerAdUnitId(),
-      size: AdSize.banner,
-      request: const AdRequest(),
-      listener: BannerAdListener(
-        onAdLoaded: (ad) {
-          setState(
-                () {
-              _ad = ad;
-            },
-          );
-        },
-        onAdFailedToLoad: (ad, error) {
-          developer.log(
-            'Enable to load the ad : ${error.message}',
-            name: 'banner-widget',
-          );
-          _ad?.dispose();
-        },
-      ),
-    );
-    _bannerAd2.load();
-  }
-
   @override
   void initState() {
-    _createBannerAd2();
+    _createBannerAd();
     super.initState();
   }
 
@@ -82,45 +56,29 @@ class _AdBannerWidgetState extends State<AdBannerWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedSwitcher(
-      duration: const Duration(milliseconds: 200),
-      transitionBuilder: (Widget child, Animation<double> animation) {
-        return FadeTransition(
-          opacity:
-          CurvedAnimation(parent: animation, curve: Curves.easeIn),
-          child: SlideTransition(
-            position: Tween(
-              begin: const Offset(-1.0, 0.0),
-              end: const Offset(0.0, 0.0),
-            ).animate(animation),
-            child: child,
-          ),
-        );
-      },
-      child: _ad != null
-          ? Card(
-        child: Container(
-          key: const ValueKey('adContent'),
-          height: 55,
-          alignment: Alignment.center,
-          child: AnimatedSize(
-            curve: Curves.ease,
-            duration: const Duration(seconds: 1),
-            child: AnimatedSwitcher(
-              duration: const Duration(seconds: 1),
-              reverseDuration: const Duration(milliseconds: 500),
-              transitionBuilder: (child, animation) {
-                return FadeTransition(
-                  opacity: animation,
-                  child: child,
-                );
-              },
-              child: AdWidget(ad: _bannerAd2),
+    return _ad != null
+        ? Card(
+            child: Container(
+              key: const ValueKey('adContent'),
+              height: 55,
+              alignment: Alignment.center,
+              child: AnimatedSize(
+                curve: Curves.ease,
+                duration: const Duration(seconds: 1),
+                child: AnimatedSwitcher(
+                  duration: const Duration(seconds: 1),
+                  reverseDuration: const Duration(milliseconds: 500),
+                  transitionBuilder: (child, animation) {
+                    return FadeTransition(
+                      opacity: animation,
+                      child: child,
+                    );
+                  },
+                  child: AdWidget(ad: _bannerAd),
+                ),
+              ),
             ),
-          ),
-        ),
-      )
-          : const SizedBox.shrink(),
-    );
+          )
+        : const SizedBox.shrink();
   }
 }
