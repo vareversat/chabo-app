ed6518629757962b7a94adb83c152e47f1920f05
Valentin REVERSAT <dev@vareversat.fr>
2026-07-04 20:13:51 +0200

    chore(deps): Upgrade all deps and Flutter
     - Flutter 3.44
     - Gradle 9

--- android/app/src/main/kotlin/fr/vareversat/chabo/MainActivity.kt
+++ android/app/src/main/kotlin/fr/vareversat/chabo/MainActivity.kt
@@ -1,19 +1,5 @@
 package fr.vareversat.chabo
 
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
 
-class MainActivity: FlutterActivity() {
-
-    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
-        super.configureFlutterEngine(flutterEngine)
-        GoogleMobileAdsPlugin.registerNativeAdFactory(
-            flutterEngine, "listTile", ListTileNativeAdFactory(context))
-    }
-
-    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
-        super.cleanUpFlutterEngine(flutterEngine)
-        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "listTile")
-    }
-}
+class MainActivity : FlutterActivity()
